3ecf223b162f5552cd6346b48a4942e7e935b122
marieooq <otakimarie@gmail.com>
2023-06-21 18:04:44 +0900

    added second prompt to get better result

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,7 +4,9 @@ const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = `Create a resume based on the input provided below.
+  Input:
+`;
 
 const generateAction = async (req, res) => {
   // Run first prompt
@@ -12,14 +14,38 @@ const generateAction = async (req, res) => {
 
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.7,
-    max_tokens: 250,
+    max_tokens: 1250,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  console.log("basePromptOutput.text", basePromptOutput.text);
+
+  const secondPrompt = `Based on work experience, create a resume that clearly highlights your accomplishments, sets the candidate apart from others, and appeals to the HR department.
+
+Job experience: ${basePromptOutput.text}
+
+The format should include these items.
+
+Name, Summary, Job Experience(includes achievements).`;
+
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
